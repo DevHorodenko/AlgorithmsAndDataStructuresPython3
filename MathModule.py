f434bebb548011d86f764fdcba512bdd5ff1b45e
Leonardo <leohorodenko@hotmail.com>
2025-06-11 10:56:33 -0300

    add some of the math module functions

---
+++ MathModule.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.ceil(3.14)) # to see the bigger int, bigger or equal the number
+
+print(math.fabs(-4)) # the absolute value from a number
+
+print(math.floor(3.14)) # bigger int less or equal
+
+print(math.exp(2))
+
+print(math.log(10)) # logarithm
+
+print(math.sqrt(25)) # square root
+
+math.cos(0) # cosine
+
+math.sin(0) # sine
