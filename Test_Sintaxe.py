a172e1b5c1c478102c93598569c2ee09baeb703a
Leonardo <leohorodenko@hotmail.com>
2025-06-11 00:24:34 -0300

    first commit

---
+++ Test_Sintaxe.py
@@ -0,0 +1,25 @@
+print("Hello")
+
+# Comentario unico
+
+"""
+Comentarios
+de
+multiplas
+linhas
+"""
+
+n = 10
+
+print(type(n)) 
+
+n = None # Mesma coisa que "null"
+
+n = "Leonardo"
+
+print(n.upper())
+
+n = "LEONARDO"
+
+print(n.lower())
+
