fbaee18c164a348ceec3e89acd84769d4e4dd5ef
Leonardo <leohorodenko@hotmail.com>
2025-06-11 00:33:43 -0300

    adds basic syntax

---
+++ Test_Syntax.py
@@ -0,0 +1,47 @@
+print("Hello")
+
+# Unique comment
+
+"""
+Multiple
+line
+comments
+"""
+
+n = 10
+
+print(type(n)) 
+
+n = None # Same as "null"
+
+n = "Leonardo"
+
+print(n.upper())
+
+n = "LEONARDO"
+
+print(n.lower())
+
+list = [1, 2, 3, 4]
+
+print(list)
+
+nameList = ['Jhon', 'Leo', 'Cesar']
+
+print(nameList)
+
+mixedList = [2, 'Leo', 3.14]
+
+print(mixedList)
+
+mixedList[1] = 'Jhon'
+
+print(mixedList)
+
+tuple = (1,2,3) # Immutable, means it cant be modified
+
+print(tuple)
+
+dic = {'Leo':24, 'Jhon':26, 'Mary':19} # Map
+
+print(dic)
