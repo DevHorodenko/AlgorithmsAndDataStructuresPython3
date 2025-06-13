b7645f54faffc6dca82efc164727d75d3244734c
Leonardo <leohorodenko@hotmail.com>
2025-06-13 11:15:56 -0300

    add heritage example

---
+++ Heritage.py
@@ -0,0 +1,33 @@
+class Transport():
+    
+    def __init__(self, name, weight, price):
+        self.name = name
+        self.weight = weight
+        self.price = price
+        pass
+
+    def getName(self):
+        return self.name
+    
+    def getWeight(self):
+        return self.weight
+    
+    def getPrice(self):
+        return self.price
+    
+class Car(Transport):
+
+    def __init__(self, name, weight, price, brand):
+        super().__init__(name, weight, price)
+        self.brand = brand
+
+    def getBrand(self):
+        return self.brand
+
+
+t = Car('Camaro', 500, 14999.99, "Chevrolet")
+
+print(t.getName())
+print(t.getWeight())
+print(t.getPrice())
+print(t.getBrand())
