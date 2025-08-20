13c4d20e4c2cb773bbf32ed40bcf213e46a16bee
Leonardo <leohorodenko@hotmail.com>
2025-08-20 13:37:07 -0300

    add example of DFS in graph that already existed

--- GraphMatrix.py
+++ GraphMatrix.py
@@ -6,7 +6,7 @@ class Graph:
     def __init__(self, vertices):
         self.vertices = vertices  # Number of vertices
         self.graph = [[0] * vertices for i in range(vertices)]  # Adjacency list representation
-
+        self.visited = [False] * vertices  # Track visited vertices for DFS
 
     def add_edge(self, u, v):
         """Add an edge to the graph."""
@@ -26,6 +26,14 @@ class Graph:
         if self.graph[u - 1][v - 1] == 1:
             return True
         return False
+    
+    def dfs(self, start):
+        """Depth First Search to traverse the graph."""
+        self.visited[start - 1] = True
+        print(f"Visited {start}")
+        for i in range(self.vertices):
+            if self.graph[start - 1][i] == 1 and not self.visited[i]:
+                self.dfs(i + 1)  # Convert to 1-based index for user-friendly output
 
 # Example usage:
 g = Graph(5)
@@ -40,3 +48,6 @@ g.show_graph() # Output will show the adjacency list representation of the graph
 
 print(g.hasConnection(1, 3))  # Output: True - 1 has connection with 3
 print(g.hasConnection(2, 4))  # Output: False - 2 has no connection with 4
+
+# This will perform a DFS starting from vertex 1.
+print(g.dfs(1))
